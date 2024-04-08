01128dcd9a65bd3e216d93962e8f07456f80fa59
Dhairya <mehra.dhairya.btech2022@sitpune.edu.in>
2024-04-08 14:00:07 +0530

    files uploaded

---
+++ JavaScript/first.js
@@ -0,0 +1,18 @@
+console.log("Helli JS!");
+console.log("Welcome to Hackathon");
+
+age = 20;
+fullName = "Dhairya A Mehra";
+console.log("Name: " + fullName + "     Age: " + age);
+
+let firstName = "SkillIssue"; // use let when you have to declare the variable and you can also use the let  instead of var
+console.log(firstName);
+
+const student = {
+  name: "Dhairya A Mehra",
+  age: 20,
+  cgpa: 7.6,
+  isPass: true,
+};
+
+console.log(student["age"]);
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  // res is a response and rq is request
+  console.log(req.url);
   res.end("Hello from the server");
+  if (req.url === "/cars") {
+    res.end("this is cars");
+  } else if (req.url === "/fruits") {
+    res.end("this is fruit");
+  } else {
+    res.end("Hello from the server.");
+  }
 });
 
-server.listen(8000, "localhost", () => {
+server.listen(8001, "localhost", () => {
   console.log("Server is Started");
 });
